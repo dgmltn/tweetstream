df2f890eed41b47791d0ddc9212387fb8b78c874
Doug Melton <doug@mobiata.com>
2016-02-10 13:18:56 -0800

    Added retweet functionality/display to tweets

--- public/javascripts/tweets.js
+++ public/javascripts/tweets.js
@@ -16,20 +16,47 @@ $(document).ready(function() {
       $(".tweet__user_profile_image").fadeOut();
       $(".tweet__time").fadeOut();
 
-      $(".tweet__text").fadeReplace(tweet.text, function(text) {
-        return text.replace(/(@\w+)/g, "<span class='username'>\$1</span>")
+      // Tweet data
+      var text = tweet.text;
+      var user = tweet.user.screen_name;
+      var profile_image_url = tweet.user.profile_image_url;
+      var created_at = tweet.created_at;
+      var via;
+      
+      // If this is a retweet, update the things
+      if ('retweeted_status' in tweet) {
+        text = tweet.retweeted_status.text;
+        via = tweet.user.screen_name;
+        user = tweet.retweeted_status.user.screen_name;
+        profile_image_url = tweet.retweeted_status.user.profile_image_url;
+        created_at = tweet.retweeted_status.created_at;
+      }
+
+      // Post processing tweet data
+      profile_image_url = profile_image_url.replace("_normal", "");
+
+      var formatter = function(text) {
+        return text
+          .replace(/(@\w+)/g, "<span class='username'>\$1</span>")
           .replace(/(#\w+)/g, "<span class='hashtag'>\$1</span>")
           .replace(/\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/i, "<span class='url'>\$1</span>");
-      }, function() {
-        $(".tweet__user").text("@" + tweet.user.screen_name);
-        var url = tweet.user.profile_image_url.replace("_normal", "");
-        $(".tweet__user_profile_image").html('<img height="96" width="96" src="' + url + '" />');
-        $(".tweet__time abbr").attr('title', tweet.created_at);
+      };
+
+      // Callback will be called between fade out and fade in
+      var callback = function() {
+        $(".tweet__user").text("@" + user);
+        if (via !== undefined) {
+            $(".tweet__user").append('<span class="via">♻' + via + '</span>');
+        }
+        $(".tweet__user_profile_image").html('<img height="96" width="96" src="' + profile_image_url + '" />');
+        $(".tweet__time abbr").attr('title', created_at);
         $(".timeago").timeago();
         $(".tweet__user").fadeIn(500);
         $(".tweet__user_profile_image").fadeIn(500);
         $(".tweet__time").fadeIn(500);
-      });
+      };
+
+      $(".tweet__text").fadeReplace(text, formatter, callback);
 
       $(".stats__pending .data").text(tweets.length);
     }
